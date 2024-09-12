a79d87e6d2a98b848b20bb1effd48f0ca00fe7a5
kshivakumar <kshivakumar@outlook.com>
2024-09-12 16:56:51 +0530

    Update build.sh, use correct env var in render.yaml

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py create_sample_data --run-only-if-no-users
